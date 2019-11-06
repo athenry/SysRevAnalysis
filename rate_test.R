a7ded6a0d3756886ca4588d9806d44490c673904
Alison Henry <alisontaylorhenry@gmail.com>
2019-11-06 14:32:26 -0700

    Initial attempt at running a Chow test on the publication data.

--- rate_test.R
+++ rate_test.R
@@ -1,4 +1,5 @@
 ## Test to compare increase of of systematic reviews in engineering literature to increase in number of publications in the same time span.
+
 ## Load needed libraries
 library(tidyverse)
 library(bibliometrix)

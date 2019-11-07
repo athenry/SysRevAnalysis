bb70c9a2e61f3ef8919e3966a174fdd0b8438065
Alison Henry <alisontaylorhenry@gmail.com>
2019-11-07 14:02:36 -0700

    changed file names

--- rate_test.R
+++ rate_test.R
@@ -2,17 +2,18 @@
 
 ## Load needed libraries
 library(tidyverse)
-library(bibliometrix)
 library(tseries)
 
 ## read in our data. We will have three time series: all journal and conference articles published from 2000-2018, the systematic review articles published in the same time period, and all the non-SR articles published in that time period.
 
 statdata <- read.csv(file = "Data/statdata.csv", header = TRUE)
-SRdata <- read.csv(file = "Data/SRquery.csv", header = TRUE)
+SRdata <- read.csv(file = "Data/SRdata.csv", header = TRUE)
 Adata <- read.csv(file = "Data/Adata.csv", header = TRUE)
 
 ## generate a basic plot to examine the data
-plot1 <- ggplot(data = SRdata, aes(Year, Count)) + geom_line(col = "red") + geom_line(data = statdata, aes(Year, Count/1000), col = "blue") + geom_line(data = Adata, aes(Year, Count/1000), col = "green") + theme_minimal()
+plot1 <- ggplot(data = SRdata, aes(Year, Count)) + geom_line(col = "red") + geom_line(data = statdata, aes(Year, Count/1000), col = "blue") + geom_point(data = Adata, aes(Year, Count/1000), col = "green") + theme_minimal()
+
+plot1
 
 ## Perform regressions on each series
 regP <- lm(formula = Count~Year, data = statdata)
@@ -24,7 +25,7 @@ rssP <- sum(residuals(regP)^2)
 rssA <- sum(residuals(regA)^2)
 rssB <- sum(residuals(regB)^2)
 
-## Chow test. Null hypothesis  = no structural break
+## Chow test. Null hypothesis  = no significant difference
 
 ## k is the number of parameters for our model
 k=2
@@ -34,7 +35,7 @@ Fcrit = qf(.95, df1=regA$df.residual, df2 = regB$df.residual)
 Fcrit
 
 ## Calculate the Chow statistic
-Chow_statistic = (((rssP - (rssA + rssB))/k)/((rssA + rssB)/(regA$df.residual + regB$df.residual - 2*k)))
+Chow_statistic = ((rssP - (rssA + rssB))/k)/((rssA + rssB)/(regA$df.residual + regB$df.residual - 2*k))
 Chow_statistic
 
 ## If the Chow statistic is larger than our F critical value, we reject the null hypothesis

ef0e072952b259f9ecaac5740f1c44f7e18dd7e1
Alison Henry <alisontaylorhenry@gmail.com>
2020-02-01 17:52:45 -0700

    added model data to plot

--- rate_test.R
+++ rate_test.R
@@ -13,38 +13,29 @@ Adata <- read.csv(file = "Data/Adata.csv", header = TRUE)
 alldata <- merge(statdata, SRdata, by = "Year")
 colnames(alldata) <- c("Year", "allpubs", "SR")
 alldata$share <- (alldata$SR / alldata$allpubs) * 100
+##alldata$time <- (alldata$Year-2000)
 
 ## generate a basic plot to examine the data
-plot1 <- ggplot(data = alldata) + geom_line(aes(Year, SR), col = "red") + geom_line(aes(Year, allpubs/1000), col = "blue") + theme_minimal()
+plot1 <- ggplot(data = alldata) + geom_point(aes(Year, SR), col = "red") + geom_point(aes(Year, allpubs/1000), col = "blue") + theme_minimal()
 plot1
 
 ## Examine the change in proportion of articles which use SR methodology
-plot2 <- ggplot(data = alldata) + geom_line(aes(Year, share), col = "green")
+plot2 <- ggplot(data = alldata) + geom_point(aes(x=Year, y=share)) + theme_minimal() 
 plot2
 
-## Perform a trend test to confirm that the share of articles using this methodology shows a pronoun
+## perform a linear regression on the share of articles 
 
-## Perform regressions on each series, omitting intercept
-regP <- lm(formula = Count~Year - 1, data = subset(statdata, Year > 2014))
-regA <- lm(formula = Count~Year - 1, data = subset(Adata, Year > 2014))
-regB <- lm(formula = Count~Year - 1, data = subset(SRdata, Year >2014))
+model <- lm(share ~ poly(Year, 2), data = alldata)
 
-## Calculate the residual sum of squares for each regression
-rssP <- sum(residuals(regP)^2)
-rssA <- sum(residuals(regA)^2)
-rssB <- sum(residuals(regB)^2)
+## examine the summary of the model
+summary(model)
 
-## Chow test. Null hypothesis  = no significant difference
+## create predicted values for our years based on the model
+df <- predict(model, data.frame(Year = alldata$Year))
+alldata$predValue <- df
 
-## k is the number of parameters for our model
-k=2
+## add the predicted values to our plot
+plot3 <- ggplot(data = alldata, aes(x=Year, y=share)) + geom_point() + geom_line(aes(x=Year, y=predValue)) + theme_minimal()
+plot3
 
-## Determine the F critical value 
-Fcrit = qf(.95, df1=regA$df.residual, df2 = regB$df.residual)
-Fcrit
 
-## Calculate the Chow statistic
-Chow_statistic = ((rssP - (rssA + rssB))/k)/((rssA + rssB)/(regA$df.residual + regB$df.residual - 2*k))
-Chow_statistic
-
-## If the Chow statistic is larger than our F critical value, we reject the null hypothesis
